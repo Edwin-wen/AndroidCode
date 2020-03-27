1c6c1f285fed7ce2bad2fe07d262060cfe1143c9
liangyaowen <liangyaowen@kingsoft.com>
2020-03-27 18:18:37 +0800

    init activity

--- app/src/main/java/com/example/edwin/knowledgecode/activity/MainActivity.java
+++ app/src/main/java/com/example/edwin/knowledgecode/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.edwin.knowledgecode;
+package com.example.edwin.knowledgecode.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.edwin.knowledgecode.R;
 
 public class MainActivity extends AppCompatActivity {
 
